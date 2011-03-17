908d5d747b346e2f29d8dd9fb3fdd48bd3887038
Sean Moon <pair+smoon@pivotallabs.com>
2011-03-17 17:26:27 -0400

    remove NY ranking algorithm

--- pivotpong.rb
+++ pivotpong.rb
@@ -8,43 +8,6 @@ def calculate_ranking
   File.readlines("data/matches.csv").each do |line|
     line.chomp!
     time, winner, loser = line.split ","
-    if (ranking.include?(winner) && ranking.include?(loser))
-      # exising winner is ranked ahead of existing loser
-      # ranks don't change
-      winner_index = ranking.index(winner)
-      loser_index = ranking.index(loser)
-      if winner_index < loser_index
-        puts "skip"
-      else
-        #both players exist, move up winner
-        ranking.delete(winner)
-        ranking.insert(loser_index, winner)
-      end
-    # exising winner is ranked ahead of non-existing loser
-    # ranks don't change
-    # loser is added to list
-    elsif (ranking.include?(winner) && !ranking.include?(loser))
-      ranking << loser
-    # exising loser, non-existing winner
-    # winner takes losers rank
-    elsif (!ranking.include?(winner) && ranking.include?(loser))
-      index = ranking.index(loser)
-      ranking.insert(index,winner)
-    else
-      # add two new players
-        #add winner, loser
-      ranking << winner
-      ranking << loser
-    end
-  end
-  ranking
-end
-
-def calculate_ranking_by_half_method
-  ranking = []
-  File.readlines("data/matches.csv").each do |line|
-    line.chomp!
-    time, winner, loser = line.split ","
 
     if !ranking.include? winner
       ranking << winner
@@ -67,13 +30,6 @@ end
 
 get "/" do
   @ranking = calculate_ranking
-  @algo = "NY"
-  haml :index
-end
-
-get "/sf" do
-  @ranking = calculate_ranking_by_half_method
-  @algo = "SF"
   haml :index
 end
 
@@ -90,10 +46,6 @@ post "/add-match" do
     File.open("data/matches.csv", "a") do |f|
       f.write "#{Time.now},#{winner},#{loser}\n"
     end
-  end
-
-  case params[:algo]
-    when "SF"; redirect '/sf'
   end
 
   redirect '/'
